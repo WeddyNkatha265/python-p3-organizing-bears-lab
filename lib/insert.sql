8c0a4427fb83da2dd0f157a3d97cefad69883528
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-09-06 15:46:42 +0300

    Updates organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+-- lib/insert.sql
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 20, "M", "Brown", "Calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 10, "M", "Black", "Aggressive", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 6, "F", "Brown", "Playful", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 11, "M", "Brown", "Stern", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 13, "F", "White", "Gentle", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 2, "M", "Black", "Grumpy", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 6, "F", "Brown", "Friendly", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 7, "M", "Brown", "Curious", 0);
